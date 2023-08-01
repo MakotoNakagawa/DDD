46e8b419a9876ecf5c2bf49e0ca2f53145dc61c3
MakotoNakagawa <makoto2.nakagawa@lixil.com>
2023-08-01 23:09:25 +0900

    モデル追加

---
+++ RazorPagesWeb/Web/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+using Web.Data;
+
+namespace Web.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Web.Data.WebContext _context;
+
+        public IndexModel(Web.Data.WebContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
